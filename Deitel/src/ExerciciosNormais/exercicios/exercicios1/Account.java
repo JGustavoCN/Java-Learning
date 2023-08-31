8d51b242d0d5010e775530d44be06c9c71f6b124
JGustavoCN <josegustavocorreianascimento@gmail.com>
2023-08-31 18:13:10 -0300

    Criptografia na Classe Account Deitel

--- Deitel/src/ExerciciosNormais/exercicios/exercicios1/Account.java
+++ Deitel/src/ExerciciosNormais/exercicios/exercicios1/Account.java
@@ -9,9 +9,14 @@ public class Account {
     // Os atributos devem estar no começo da classe
     private String name;
     private double balance;
+    private static final int TAMANHO_DA_SENHA = 4;
+    private int senha;
 
-    public Account(String name, double balance) {
+    public Account(String name, int senha, double balance) {
         this.name = name;
+        if (senha > 999 && senha < 10_000) {
+            this.senha = senha;
+        }
         if (balance > 0) {
             this.balance = balance;
         }
@@ -36,18 +41,140 @@ public class Account {
     public void deposit(double deposit) {
         if (deposit > 0) {
             this.setBalance(this.balance + deposit);
+        } else {
+            System.out.println("erro: Valor negativo ou nulo");
         }
 
     }
 
     public void withdraw(double withdraw) {
-       
-        if (withdraw < 0 || withdraw > this.balance) {
-            System.out.println("Valor invalido: Não Muito acima ou muito a baixo do saldo");
-            return;
-        }
-        this.setBalance(this.balance - withdraw);
 
+        if (withdraw <= 0 || withdraw > this.balance) {
+            System.out.println("Erro: Valor invalido: Muito acima, negativo ou nulo");
+
+        } else {
+            this.setBalance(this.balance - withdraw);
+        }
+
+    }
+
+    public int importarSenha() {
+        int vetor[] = new int[TAMANHO_DA_SENHA];
+        int j = 0;
+        int criptografado = 0;
+        for (int i = (TAMANHO_DA_SENHA - 1); i >= 0; i--) {
+            int casa = (int) Math.pow(10, i);
+            int casa2 = (this.senha / casa) % 10;
+            vetor[j] = ((casa2 + 7) % 10);
+            j++;
+        }
+        for (int i = 0; i < TAMANHO_DA_SENHA; i++) {
+            int troca;
+            switch (i) {
+                case 0:
+                    troca = vetor[i];
+                    vetor[i] = vetor[i + 2];
+                    vetor[i + 2] = troca;
+                    break;
+                case 1:
+                    troca = vetor[i];
+                    vetor[i] = vetor[i + 2];
+                    vetor[i + 2] = troca;
+                    break;
+                default:
+                    break;
+            }
+        }
+        for (int i = 0; i < TAMANHO_DA_SENHA; i++) {
+            switch (i) {
+                case 0:
+                    criptografado += vetor[i] * 1000;
+                    break;
+                case 1:
+                    criptografado += vetor[i] * 100;
+                    break;
+                case 2:
+                    criptografado += vetor[i] * 10;
+                    break;
+                case 3:
+                    criptografado += vetor[i];
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return criptografado;
+    }
+
+    public static void verSenha(int criptografado, int senhaAdministracao) {
+        int vetor[] = new int[TAMANHO_DA_SENHA];
+        int j = 0;
+        if (senhaAdministracao != 12345) {
+            System.out.println("Senha da Administracao esta incorreta");
+        } else {
+            for (int i = (TAMANHO_DA_SENHA - 1); i >= 0; i--) {
+                int casa = (int) Math.pow(10, i);
+                int casa2 = (criptografado / casa) % 10;
+                vetor[j] = casa2;
+                j++;
+            }
+
+            for (int i = 0; i < TAMANHO_DA_SENHA; i++) {
+                int troca;
+                switch (i) {
+                    case 0:
+                        troca = vetor[i];
+                        vetor[i] = vetor[i + 2];
+                        vetor[i + 2] = troca;
+                        break;
+                    case 1:
+                        troca = vetor[i];
+                        vetor[i] = vetor[i + 2];
+                        vetor[i + 2] = troca;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            for (int i = 0; i < TAMANHO_DA_SENHA; i++) {
+                switch (vetor[i]) {
+                    case 1:
+                        vetor[i] = 4;
+                        break;
+                    case 2:
+                        vetor[i] = 5;
+                        break;
+                    case 3:
+                        vetor[i] = 6;
+                        break;
+                    case 4:
+                        vetor[i] = 7;
+                        break;
+                    case 5:
+                        vetor[i] = 8;
+                        break;
+                    case 6:
+                        vetor[i] = 9;
+                        break;
+                    case 7:
+                        vetor[i] = 0;
+                        break;
+                    case 8:
+                        vetor[i] = 1;
+                        break;
+                    case 9:
+                        vetor[i] = 2;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            for (int k : vetor) {
+                System.out.print(k);
+            }
+        }
     }
 
 }
--- Deitel/src/ExerciciosNormais/exercicios/exercicios1/AccountTest.java
+++ Deitel/src/ExerciciosNormais/exercicios/exercicios1/AccountTest.java
@@ -3,6 +3,7 @@ package ExerciciosNormais.exercicios.exercicios1;
 //Não precisa do import, pois estao na mesma classe
 import ExerciciosNormais.exercicios.exercicios1.Account;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,18 +15,19 @@ import java.util.Scanner;
 public class AccountTest {
 
     public static void main(String[] args) {
+        /*
         Account account1 = new Account("Jane Green", 50.00);
         Account account2 = new Account("John Blue", -7.53);
 
-    // exibe saldo inicial de cada objeto
+        // exibe saldo inicial de cada objeto
         System.out.printf("%s balance: $%.2f %n",
                 account1.getName(),
-                 account1.getBalance());
+                account1.getBalance());
         System.out.printf("%s balance: $%.2f %n%n",
                 account2.getName(),
-                 account2.getBalance());
+                account2.getBalance());
         // cria um Scanner para obter entrada a partir da janela de comando
-        Scanner input = new Scanner(System.in);
+        
 
         System.out.print("Enter deposit amount for account1: "); // prompt
         double depositAmount = input.nextDouble(); // obtém a entrada do usuárioÿ
@@ -36,10 +38,10 @@ public class AccountTest {
         // exibe os saldos
         System.out.printf("%s balance: $%.2f %n",
                 account1.getName(),
-                 account1.getBalance());
+                account1.getBalance());
         System.out.printf("%s balance: $%.2f %n%n",
                 account2.getName(),
-                 account2.getBalance());
+                account2.getBalance());
 
         System.out.print("Enter deposit amount for account2: "); // prompt
         depositAmount = input.nextDouble(); // obtém a entrada do usuárioÿ
@@ -50,10 +52,51 @@ public class AccountTest {
         // exibe os saldos
         System.out.printf("%s balance: $%.2f %n",
                 account1.getName(),
-                 account1.getBalance());
+                account1.getBalance());
         System.out.printf("%s balance: $%.2f %n%n",
                 account2.getName(),
-                 account2.getBalance());
+                account2.getBalance());
+         */
+
+        Account conta1 = new Account("Gustavo",1245,100)
+                , conta2 = null;
+//        displayAccount(conta1);
+        System.out.println(conta1.importarSenha());
+        Account.verSenha(conta1.importarSenha(), 12345);
 
     }
+
+    public static void displayAccount(Account accountToDisplay) {
+
+        String nome = JOptionPane.showInputDialog(
+                null,
+                "Digite o nome da titular: ",
+                "Criacao da Conta",
+                JOptionPane.INFORMATION_MESSAGE
+        );
+
+        int senha = Integer.parseInt(
+                JOptionPane.showInputDialog(
+                        null,
+                        "Digite a senha de 4 digitos: : ",
+                        "Criacao da Conta",
+                        JOptionPane.INFORMATION_MESSAGE
+                )
+        );
+        double deposito = Double.parseDouble(
+                JOptionPane.showInputDialog(
+                        null,
+                        "Qual o valor do primeiro deposito: ",
+                        "Criacao da Conta",
+                        JOptionPane.INFORMATION_MESSAGE
+                )
+        );
+
+        accountToDisplay = new Account(nome, senha, deposito);
+        System.out.printf("%s balance: $%.2f %n",
+                accountToDisplay.getName(),
+                accountToDisplay.getBalance()
+        );
+    }
+
 }
