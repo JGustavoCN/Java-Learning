d13a6ce05e8c004a3bc25192feb5bd90bd9095b9
JGustavoCN <josegustavocorreianascimento@gmail.com>
2024-03-11 18:18:07 -0300

    ED - Matriz repeticoes

--- Faculdade/src/br/edu/ifs/ed/AppED.java
+++ Faculdade/src/br/edu/ifs/ed/AppED.java
@@ -9,30 +9,80 @@ import java.util.Vector;
 public class AppED {
 
     public static void main(String[] args) {
-        
-
+        testeMatriz();
     }
 
-    /*
+    /* TENTAR PEGAR OS VALORES REPETIDOS E SEPARAR UMA COPIA EM UM NOVO VETOR E 
+    TBM ARMAZENAR A QUANTIDADE DE REPETICOES DE CADA
+    
         Crie em Java uma matriz 3x5 de inteiros,
 preencha a matriz e depois:
 a) Informe se a matriz possui elementos
 repetidos;
 b) A quantidade de números pares;
 c) A quantidade de números ímpares
-    */
-    
-    public static void testeMatriz(){
+     */
+    public static void testeMatriz() {
         int[][] matriz = new int[3][5];
+        int indiceVetorMatriz = 0;
+        int[] vetorMatriz = new int[3 * 5];
+        int[] vetorRepetidos = new int[3 * 5];
+        int countRepetidos = 0;
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz.length; j++) {
-                System.out.print("matriz["+i+"]["+j+"]: ");
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print("matriz[" + i + "][" + j + "]: ");
                 matriz[i][j] = Entrada.teclado.nextInt();
+                vetorMatriz[indiceVetorMatriz] = matriz[i][j];
+                indiceVetorMatriz++;
             }
-            System.out.println("");
+            
         }
+        int totalDeValores = matriz.length * matriz[0].length;
+        int marcarLinha = 0;
+        int quantPares = 0;
+        int quantImpares = 0;
+        for (int i = 0; i < totalDeValores; i++) {
+            
+            int linha = i / matriz[0].length;
+            int coluna = i % matriz[0].length;
+            
+            if (linha != marcarLinha) {
+                System.out.println("");
+                marcarLinha++;
+            }
+            System.out.print(matriz[linha][coluna] + " ");
+            
+            if (matriz[linha][coluna] % 2 == 0){
+                quantPares++;
+            }else{
+                quantImpares++;
+            }
+            
+        }
+        
+        boolean temRepetido = false; 
+        for (int i = 0; i < vetorMatriz.length; i++) {
+            
+            for (int j = 0; j < vetorMatriz.length; j++) {
+                if (vetorMatriz[i] == vetorMatriz[j]) {
+                    for (int k = 0; k < vetorRepetidos.length; k++) {
+                        
+                    }
+                    temRepetido = true;
+                }
+            }
+            
+        }
+        System.out.println("\nTem repetido? "+ ((temRepetido)?"Sim":"Não"));
+        System.out.println("Quantidade pares: "+ quantPares);
+        System.out.println("Quantidade impares: "+ quantImpares);
+        System.out.println("\nQuantidade de repetidos: "+ countRepetidos);
+        for (int repetido : vetorRepetidos) {
+            System.out.print(repetido+" ");
+        }
+        
     }
-    
+
     public static void testeVetorAluno() {
         Aluno[] turma = new Aluno[2];
         String nome;
@@ -53,15 +103,15 @@ c) A quantidade de números ímpares
         for (Aluno aluno : turma) {
             System.out.println(aluno.getNome() + " está com média " + aluno.calcularMedia());
         }
-        
+
         System.out.println("== Procurar aluno");
         System.out.print("Qual o nome do aluno: ");
         nome = Entrada.teclado.next();
         for (Aluno aluno : turma) {
             if (aluno.getNome().equalsIgnoreCase(nome)) {
-                System.out.println("Aluno: "+ aluno.getNome()+" -> Média: "+ aluno.calcularMedia());
+                System.out.println("Aluno: " + aluno.getNome() + " -> Média: " + aluno.calcularMedia());
             }
-            
+
         }
         int alunosAbaixoDaMedia = 0;
         for (Aluno aluno : turma) {
@@ -69,8 +119,8 @@ c) A quantidade de números ímpares
                 alunosAbaixoDaMedia++;
             }
         }
-        System.out.println("Quantidade de alunos abaixo da média: "+ alunosAbaixoDaMedia);
-        
+        System.out.println("Quantidade de alunos abaixo da média: " + alunosAbaixoDaMedia);
+
     }
 
     public static void testeVetorAutomovel() {
